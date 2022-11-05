96b570eff23e1b1ba6f408a72b65683abdfc8852
gr3gor1 <115941066+gr3gor1@users.noreply.github.com>
2022-11-05 10:59:38 +0200

    Update shape_calculator.py

--- polygon-area-calculator/shape_calculator.py
+++ polygon-area-calculator/shape_calculator.py
@@ -1,29 +1,36 @@
 class Rectangle:
   width = 0
   height = 0
-
+  
+  #initialize class with specific width and height
   def __init__(self,wid,hei):
     self.width = wid
     self.height = hei
-
+  
+  #set width
   def set_width(self,wid):
     self.width = wid
-
+  
+  #set height
   def set_height(self,hei):
     self.height = hei
-
+  
+  #get area value
   def get_area(self):
     area = self.width * self.height
     return area
-    
+  
+  #get perimeter value   
   def get_perimeter(self):
     perimeter = 2 * self.width + 2 * self.height
     return perimeter
-
+  
+  #get diagonal of the shape
   def get_diagonal(self):
     diagonal=(self.width ** 2 + self.height ** 2) ** .5
     return diagonal
 
+  #get the picture of the shape
   def get_picture(self):
     string=[]
     if(self.width>=50 | self.height>=50):
@@ -36,6 +43,8 @@ class Rectangle:
     str = ''.join(string)
     return str
 
+  # Takes another shape (square or rectangle) as an argument. 
+  # Returns the number of times the passed in shape could fit inside the shape (with no rotations).
   def get_amount_inside(self,object):
     ins = self.get_area()
     area = object.get_area()
@@ -43,6 +52,7 @@ class Rectangle:
     res = (ins - modulo) / area
     return res
 
+  #print width and height
   def __repr__(self):
     return "Rectangle(width="+str(self.width)+", height="+str(self.height)+")"
 
